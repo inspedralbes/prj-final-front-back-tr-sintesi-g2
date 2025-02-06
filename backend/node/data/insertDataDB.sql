3f4c5540ae0f8194d596773cc2138645faf1e094
Edson <a23edstorcev@inspedralbes.cat>
2025-02-06 10:40:27 +0100

    funcional

--- backend/node/data/insertDataDB.sql
+++ backend/node/data/insertDataDB.sql
@@ -25,10 +25,10 @@ VALUES
 
 -- Insertar en la tabla GAME
 INSERT INTO GAME 
-(id_player, id_inventory, game_name, game_status, total_progress, level_reached, last_save_date, position_x, position_y, health, coins, time_played) 
+(id_player, id_inventory, game_name, game_status, total_progress, last_save_date, position_x, position_y, health, coins, time_played) 
 VALUES 
-(1, 1, 'Adventure Quest', 'active', 20.00, 5, NOW(), 10.5, 3.2, 70, 50, 300),
-(2, 2, 'Warrior’s Path', 'completed', 100.00, 10, NOW(), 15.0, 8.5, 80, 200, 1500);
+(1, 1, 'Adventure Quest', 'active', 20.00, NOW(), 10.5, 3.2, 70, 50, 300),
+(2, 2, 'Warrior’s Path', 'completed', 100.00, NOW(), 15.0, 8.5, 80, 200, 1500);
 
 
 -- Insertar en la tabla ENEMY
