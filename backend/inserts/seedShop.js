dbb16ccc445fe36f495c4c847459408c608ed390
a23poldiabel <a23poldiabel@inspedralbes.cat>
2025-04-22 12:21:00 +0200

    test

--- backend/inserts/seedShop.js
+++ backend/inserts/seedShop.js
@@ -8,6 +8,13 @@ const seedShop = async () => {
       price: 500,
       image_url: 'imagenes/shop/john/idle.jpg',
       is_available: true
+    },
+    {
+      skin_name: 'test',
+      description: 'Un skin de prueba.',
+      price: 1000,
+      image_url: 'imagenes/shop/test/idle.jpg',
+      is_available: true
     }
   ];
 
